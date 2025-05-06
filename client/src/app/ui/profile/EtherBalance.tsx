8debd795eea6f50dd69195ce33f5815a3850bee5
Precious Adeyinka <pflash90@gmail.com>
2025-05-06 02:52:29 +0100

    refactor: remove disabled attribute from div element in EtherBalance component

--- client/src/app/ui/profile/EtherBalance.tsx
+++ client/src/app/ui/profile/EtherBalance.tsx
@@ -110,7 +110,6 @@ export default function EtherBalance({
           {formatNumber(Number(formatEther(balance || BigInt(0))))}
         </h4>
         <div
-          disabled={loading}
           onClick={withdrawEther}
           className={clsx(
             "rounded-full flex items-center justify-center text-white bg-purple-500 px-3 py-2 text-[10px] cursor-pointer",
